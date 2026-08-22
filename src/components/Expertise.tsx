72f8c8de06a4873152169ff188b468d9a6a88808
AgniAditya <agniaditya05@gmail.com>
2026-08-23 00:30:04 +0530

    change skills

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -11,22 +11,17 @@ import {
   faLaptopCode,
 } from "@fortawesome/free-solid-svg-icons";
 
-const languages = ["Java", "JavaScript", "TypeScript"];
+const languages = ["Java", "JavaScript", "SQL"];
 
-const frontend = ["React.js", "TailwindCSS", "Redux Toolkit", "Vite", "Axios"];
+const frontend = ["React.js", "TailwindCSS", "Redux Toolkit"];
 
-const backend = [
-  "Spring Boot",
-  "Spring Data JPA",
-  "REST API Design",
-  "JavaMail",
-];
+const backend = ["Spring Boot", "Spring Data JPA", "REST API Design"];
 
-const database = ["PostgreSQL", "MySQL", "Redis", "Apache Kafka"];
+const database = ["PostgreSQL", "Redis", "Apache Kafka"];
 
 const tools_and_devOps = [
   "Git",
-  "Docker",
+  "Github",
   "Postman",
   "Maven",
   "Event-Driven Architecture",
@@ -39,7 +34,6 @@ const tools_and_devOps = [
 const concepts = [
   "Data Structures & Algorithms",
   "Object-Oriented Programming",
-  "Operating Systems",
 ];
 
 function Expertise() {
@@ -51,10 +45,6 @@ function Expertise() {
           <div className="skill">
             <FontAwesomeIcon icon={faLaptopCode} size="3x" />
             <h3>Languages</h3>
-            <p>
-              Skilled in core programming concepts with experience writing
-              clean, maintainable code.
-            </p>
             <div className="flex-chips">
               {/* <span className="chip-title">Tech stack:</span> */}
               {languages.map((label, index) => (
@@ -65,11 +55,6 @@ function Expertise() {
           <div className="skill">
             <FontAwesomeIcon icon={faCode} size="3x" />
             <h3>Frontend</h3>
-            <p>
-              Skilled in building responsive user interfaces using React.js,
-              Tailwind CSS, and Vite with clean and efficient frontend
-              development practices.
-            </p>
             <div className="flex-chips">
               {/* <span className="chip-title">Tech stack:</span> */}
               {frontend.map((label, index) => (
@@ -80,10 +65,6 @@ function Expertise() {
           <div className="skill">
             <FontAwesomeIcon icon={faServer} size="3x" />
             <h3>Backend</h3>
-            <p>
-              Built robust backend systems with clean architecture, scalable and
-              secure RESTful APIs, and seamless integrations.
-            </p>
             <div className="flex-chips">
               {/* <span className="chip-title">Tech stack:</span> */}
               {backend.map((label, index) => (
@@ -94,10 +75,6 @@ function Expertise() {
           <div className="skill">
             <FontAwesomeIcon icon={faDatabase} size="3x" />
             <h3>Databases, Cache & Messaging</h3>
-            <p>
-              Familiar with relational and non-relational databases, including
-              schema design and query writing.
-            </p>
             <div className="flex-chips">
               {/* <span className="chip-title">Tech stack:</span> */}
               {database.map((label, index) => (
@@ -108,10 +85,6 @@ function Expertise() {
           <div className="skill">
             <FontAwesomeIcon icon={faScrewdriverWrench} size="3x" />
             <h3>Dev Tools & Concepts</h3>
-            <p>
-              Familiar with version control, and API testing concepts, with a
-              basic understanding of development workflows.
-            </p>
             <div className="flex-chips">
               {/* <span className="chip-title">Tech stack:</span> */}
               {tools_and_devOps.map((label, index) => (
@@ -122,10 +95,6 @@ function Expertise() {
           <div className="skill">
             <FontAwesomeIcon icon={faArrowsToCircle} size="3x" />
             <h3>CS Fundamentals</h3>
-            <p>
-              Strong in DSA, OOP, and OS with ability to build efficient
-              solutions.
-            </p>
             <div className="flex-chips">
               {/* <span className="chip-title">Tech stack:</span> */}
               {concepts.map((label, index) => (
